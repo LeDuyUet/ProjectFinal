73a348279fc3db43586487a998e8a37727e22b5d
LeDuyUet <100574274+LeDuyUet@users.noreply.github.com>
2023-12-26 16:58:08 +0700

    xong

--- app/src/main/java/com/example/projectfinal/MainActivity2.java
+++ app/src/main/java/com/example/projectfinal/MainActivity2.java
@@ -28,10 +28,9 @@ public class MainActivity2 extends AppCompatActivity {
             startActivity(intent);
         });
         listQuizz = new ArrayList<>();
-        listQuizz.add(new Quizz("Lịch sử\nCác câu hỏi về sự kiện liên quan đến con người", R.drawable.unnamed,1));
-        listQuizz.add(new Quizz("Địa Lý\nCác câu hỏi về vùng đất, địa hình, dân cư và các hiện tượng trên Trái Đất", R.drawable.b4c0e82a_6b49_40aa_820b_41a5afa2824c_900_900,2));
-        listQuizz.add(new Quizz("Khoa Học\nCác câu hỏi về những định luật, cấu trúc và cách vận hành của thế giới tự nhiên", R.drawable.images,3));
-        listQuizz.add(new Quizz("Nghệ thuật\nCác câu hỏi liên quan đến nghệ thuật, hội họa, kiến trúc", R.drawable.hoi_hoa_193552, 4));
+        listQuizz.add(new Quizz("Lịch sử\nCác câu hỏi về sự kiện liên quan đến con người", R.drawable.history,1));
+        listQuizz.add(new Quizz("Địa Lý\nCác câu hỏi về vùng đất, địa hình, dân cư và các hiện tượng trên Trái Đất", R.drawable.geographical,2));
+        listQuizz.add(new Quizz("Khoa Học\nCác câu hỏi về những định luật, cấu trúc và cách vận hành của thế giới tự nhiên", R.drawable.science,3));
         listQuizzAdapter = new ListQuizzAdapter(listQuizz);
         listViewQuizz = findViewById(R.id.listQuizz);
         listViewQuizz.setAdapter(listQuizzAdapter);
